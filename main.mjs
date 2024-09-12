994ec6aa9997688e5d33d6b41a6a829849983043
Bill Gao <Bill@Not-yours.local>
2024-09-12 11:36:31 -0700

    setting up express

--- main.mjs
+++ main.mjs
@@ -2,25 +2,37 @@ import axios from 'axios'
 import 'dotenv/config'
 import formData from 'form-data'
 import Mailgun from 'mailgun.js'
+import express from 'express'
 
-const pageHTML = await axios.get("https://www.dbs.com/sailing/index.html")
+const app = express()
+const port = 3000
 
-const bookingMonth = pageHTML.data.search("September Registration") 
+app.get('/crawl', async (req, res) => {
+  res.send('Crawling...')
 
-if (bookingMonth === -1) {
-    console.log(process.env.M)
-    const mailgun = new Mailgun(formData);
-    const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API});
-    
-    mg.messages.create('sandboxa7923d6c145d4897b4de2209605b8364.mailgun.org', {
-        from: "Mailgun Email <mailgun@sandboxa7923d6c145d4897b4de2209605b8364.mailgun.org>",
-        to: ["wbillgao@gmail.com"],
-        subject: "DBS Sailing Booking Month",
-        text: "It is not booking for September anymore",
-        html: `<h1> Booking month is not September/changed! </h1> 
-        <a href='https://www.dbs.com/sailing/index.html'> DBS Link </a>`
-    })
-    .then(msg => console.log(msg)) // logs response data
-    .catch(err => console.log(err)); // logs any error  
-}
+  const pageHTML = await axios.get("https://www.dbs.com/sailing/index.html")
 
+  const bookingMonth = pageHTML.data.search("September Registration") 
+  
+  if (bookingMonth === -1) {
+      console.log(process.env.M)
+      const mailgun = new Mailgun(formData);
+      const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API});
+      
+      mg.messages.create('sandboxa7923d6c145d4897b4de2209605b8364.mailgun.org', {
+          from: "Mailgun Email <mailgun@sandboxa7923d6c145d4897b4de2209605b8364.mailgun.org>",
+          to: ["wbillgao@gmail.com"],
+          subject: "DBS Sailing Booking Month",
+          text: "It is not booking for September anymore",
+          html: `<h1> Booking month is not September/changed! </h1> 
+          <a href='https://www.dbs.com/sailing/index.html'> DBS Link </a>`
+      })
+      .then(msg => console.log(msg)) // logs response data
+      .catch(err => console.log(err)); // logs any error  
+  }
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
